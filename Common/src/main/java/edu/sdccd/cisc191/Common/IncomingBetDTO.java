2778b5d3042bf3c1c1ab884343879152ee12d976
Rabit3 <kevin.vinod07@gmail.com>
2025-05-31 18:20:38 -0700

    Update IncomingBetDTO.java

--- Common/src/main/java/edu/sdccd/cisc191/Common/IncomingBetDTO.java
+++ Common/src/main/java/edu/sdccd/cisc191/Common/IncomingBetDTO.java
@@ -4,16 +4,6 @@ import lombok.Data;
 
 /**
  * Data Transfer Object (DTO) representing an incoming bet.
- * <p>
- * This class is used to encapsulate information about a user's bet request,
- * including the ID of the game being bet on, the team being bet on,
- * the amount of the bet, and the potential win amount.
- * </p>
- *
- * <p>
- * Lombok's {@code @Data} annotation automatically generates boilerplate code
- * such as getters, setters, {@code toString()}, {@code equals()}, and {@code hashCode()} methods.
- * </p>
  */
 @Data
 public class IncomingBetDTO {
@@ -40,16 +30,14 @@ public class IncomingBetDTO {
 
     /**
      * Constructs a new {@code IncomingBetDTO} with the specified values.
-     *
-     * @param gameId  the ID of the game
-     * @param betTeam the team being bet on
-     * @param betAmt  the amount being bet
-     * @param winAmt  the potential winnings
      */
     public IncomingBetDTO(Long gameId, String betTeam, int betAmt, int winAmt) {
         this.gameId = gameId;
         this.betTeam = betTeam;
         this.betAmt = betAmt;
         this.winAmt = winAmt;
+        // TODO: Make sure values like betAmt and winAmt aren't negative
     }
+
+    // TODO: Add default constructor if some tools or libraries need it
 }
